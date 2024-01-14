0d325042e19595ca7fc7657fa5a1f3243a15803b
st662 <tambolimohdshahrukh@gmail.com>
2024-01-14 12:56:37 +0530

    Terraform folder added

---
+++ terraform/0-provider.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.39"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraformstate0512"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    #   # For State Locking
+    #   dynamodb_table = "ekscluster" 
+  }
+  
+}
---
+++ terraform/11-autoscaler-iam.tf
@@ -0,0 +1,52 @@
+data "aws_iam_policy_document" "autoscaling_policy" {
+  statement {
+    actions = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions",
+        ]
+        resources = ["*"]
+        effect  = "Allow"
+    }
+  }
+
+resource "aws_iam_policy" "autoscaling_policy" {
+  name        = "AmazonEKSAutoscalerPolicy"
+  description = "IAM policy for autoscaling"
+
+  policy = data.aws_iam_policy_document.autoscaling_policy.json
+}
+
+resource "aws_iam_role" "autoscaling_role" {
+  name = "AmazonEKSClusterAutoscalerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "autoscaling.amazonaws.com",
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:cluster-autoscaler"
+          }
+        }
+      },
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "amazon_eks_autoscaler" {
+  role       = aws_iam_role.autoscaling_role.name
+  policy_arn = aws_iam_policy.autoscaling_policy.arn
+}
+output "autoscaler_role_arn" {
+  value = aws_iam_role.autoscaling_role.arn
+}
