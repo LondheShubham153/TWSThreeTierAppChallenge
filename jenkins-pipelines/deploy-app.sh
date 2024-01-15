e53ae613084a25547b67ba256b7f167ffdf5e0a6
Aniket Kale <57555096+Aniket-d-d@users.noreply.github.com>
2024-01-15 10:08:26 +0530

    add --override-existing-serviceaccounts

--- jenkins-pipelines/deploy-app.sh
+++ jenkins-pipelines/deploy-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-eksctl create iamserviceaccount --cluster=three-tier-cluster --namespace=kube-system --name=aws-load-balancer-controller --role-name AmazonEKSLoadBalancerControllerRole --attach-policy-arn=arn:aws:iam::ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy --approve --region=us-west-2
+eksctl create iamserviceaccount --cluster=three-tier-cluster --namespace=kube-system --name=aws-load-balancer-controller --role-name AmazonEKSLoadBalancerControllerRole --attach-policy-arn=arn:aws:iam::ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy --approve --region=us-west-2 --override-existing-serviceaccounts
