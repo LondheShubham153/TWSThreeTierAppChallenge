36864694c4f8b989796600bdf8d7309bdb8ad763
Aniket Kale <57555096+Aniket-d-d@users.noreply.github.com>
2024-01-15 09:23:04 +0530

    removed quoate-push-images-pipelines.sh

--- jenkins-pipelines/push-images-pipeline.sh
+++ jenkins-pipelines/push-images-pipeline.sh
@@ -3,8 +3,7 @@
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/ALIAS_INFO
 docker tag three-tier-frontend:latest public.ecr.aws/ALIAS_INFO/three-tier-frontend:latest
 docker push public.ecr.aws/ALIAS_INFO/three-tier-frontend:latest
-echo "Frontend Image Pushed Successfully
-				 
+echo Frontend Image Pushed Successfully 		 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/ALIAS_INFO
 docker tag three-tier-backend:latest public.ecr.aws/ALIAS_INFO/three-tier-backend:latest
 docker push public.ecr.aws/ALIAS_INFO/three-tier-backend:latest
