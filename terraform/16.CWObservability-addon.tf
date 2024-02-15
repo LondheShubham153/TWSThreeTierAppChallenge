7930aa70438d78e4e4e54d455fbf60c75692af42
st662 <tambolimohdshahrukh@gmail.com>
2024-02-15 19:57:13 +0530

    elk-CWObsrv added

--- terraform/16.CWObservability-addon.tf
+++ terraform/16.CWObservability-addon.tf
@@ -1,6 +1,6 @@
-resource "aws_eks_addon" "cloudwatch_observability" {
-  cluster_name             = aws_eks_cluster.demo.name
-  addon_name               = "amazon-cloudwatch-observability"
-  addon_version            = "v1.2.0-eksbuild.1"
-  service_account_role_arn = aws_iam_role.eks_cloudwatch_observability.arn
-}
+# resource "aws_eks_addon" "cloudwatch_observability" {
+#   cluster_name             = aws_eks_cluster.demo.name
+#   addon_name               = "amazon-cloudwatch-observability"
+#   addon_version            = "v1.2.0-eksbuild.1"
+#   service_account_role_arn = aws_iam_role.eks_cloudwatch_observability.arn
+# }
